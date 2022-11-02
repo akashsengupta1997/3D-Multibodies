b04e86da2ca0a8b02bf13209c399557acee547cb
Akash Sengupta <akash.sengupta1997@gmail.com>
2022-11-02 18:12:40 +0000

    Update hmr_condspin.py

--- src/networks/spin/hmr_condspin.py
+++ src/networks/spin/hmr_condspin.py
@@ -299,7 +299,7 @@ class HMR_CondSpin(nn.Module):
 
     def forward(self, x, init_pose=None, init_shape=None, init_cam=None, n_iter=3):
         xf_spin, xf_extra = self.encode(x)
-        print(torch.isnan(xf_spin).sum(), torch.isnan(xf_extra).sum())
+        print(torch.isnan(xf_spin).sum(), torch.isnan(xf_extra).sum(), xf_extra)
         xfm = torch.cat([xf_spin, xf_extra], dim=1)
         print("Num NaN: {0}".format(~(~torch.isnan(xfm)).sum()))
         hmr_results = self.decode_smpl(xfm, n_iter=n_iter)
